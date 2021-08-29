8473d08e3e4f507ad3fd688e5350d42c082a1fa2
joaoaugust2019 <jhonaugustjunior@gmail.com>
2021-08-29 17:28:28 -0300

    add table logs

--- env.ts
+++ env.ts
@@ -15,9 +15,14 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-  HOST: Env.schema.string({ format: 'host' }),
   PORT: Env.schema.number(),
+  HOST: Env.schema.string({ format: 'host' }),
+  NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
-  NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+  MYSQL_HOST: Env.schema.string({ format: 'host' }),
+  MYSQL_PORT: Env.schema.number(),
+  MYSQL_USER: Env.schema.string(),
+  MYSQL_PASSWORD: Env.schema.string.optional(),
+  MYSQL_DB_NAME: Env.schema.string(),
 })
